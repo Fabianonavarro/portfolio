367885385b7a74fe7f508a1bec6ea33b05f11526
Fabianonavarro <126501633+Fabianonavarro@users.noreply.github.com>
2026-01-27 20:49:53 -0300

    Update enviar_email.js

--- assets/enviar_email.js
+++ assets/enviar_email.js
@@ -1,30 +1,22 @@
-// Inicializa o EmailJS após o carregamento do script
-window.onload = function() {
-  // Verifica se o emailjs foi carregado corretamente
+document.addEventListener("DOMContentLoaded", function() {
   if (typeof emailjs !== "undefined") {
-    // Inicializa o EmailJS com a sua chave pública
-    emailjs.init("UF1QSKpNpI0WKgweL");  // Substitua com a sua chave pública
+    emailjs.init("UF1QSKpNpI0WKgweL");
 
-    // Captura o envio do formulário
-    document.getElementById("subscriptionForm").addEventListener("submit", function(event) {
-      event.preventDefault(); // Impede o envio padrão do formulário
+    const form = document.getElementById("subscriptionForm");
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
 
-      // Envia o formulário para o EmailJS usando o método sendForm
       emailjs.sendForm("nav.info.suporte", "formulario", this)
-        .then(function(response) {
-          // Mensagem de sucesso
+        .then(response => {
           alert("Mensagem enviada com sucesso!");
-          // Limpa os campos do formulário após o envio bem-sucedido
-          document.getElementById("subscriptionForm").reset();
-          console.log('Mensagem enviada com sucesso:', response);
-        }, function(error) {
-          // Mensagem de erro
+          form.reset();
+          console.log("Mensagem enviada:", response);
+        }, error => {
           alert("Erro ao enviar a mensagem. Tente novamente.");
-          console.error("Erro ao enviar a mensagem:", error);
-          console.log('Detalhes do erro:', error);
+          console.error("Erro:", error);
         });
     });
   } else {
-    console.error("Erro: EmailJS não foi carregado corretamente.");
+    console.error("EmailJS não carregou corretamente.");
   }
-};
+});
